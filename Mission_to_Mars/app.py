b1a7cc1645fda3ec2edecfcccb79e93ad22d7490
ktaufer <56054046+ktaufer@users.noreply.github.com>
2020-03-20 15:17:44 -0600

    close

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,28 @@
+# dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# create the flask app
+app = Flask(__name__, template_folder='templates')
+
+# declare the database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
+
+# the base route
+@app.route('/')
+def home():
+    mars = mongo.db.mars_facts.find_one()
+    return render_template('index.html', mars=mars)
+
+# the scraping route, which redirects to the base route
+@app.route('/scrape')
+def scraper():
+    mars_facts = mongo.db.mars_facts
+    mars_scrape = scrape_mars.scrape()
+    mars_facts.update({}, mars_scrape, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -5,7 +5,13 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 
-def scrape_mars():
+def init_browser():
+    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
+    return Browser('chrome', **executable_path, headless=False)
+
+def scrape():
+    browser = init_browser()
+
     # # Scrape NASA website for latest Mars news
     url1 = 'https://mars.nasa.gov/news/'
     response = requests.get(url1)
@@ -15,10 +21,8 @@ def scrape_mars():
     time.sleep(2)
 
     # # Scrape NASA for feateured Mars image URL
-    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    browser.visit(url)
+    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url2)
     button = browser.find_by_id('full_image')
     button.click()
     button2 = browser.find_by_text('more info     ')
@@ -28,89 +32,62 @@ def scrape_mars():
     time.sleep(2)
     
     # # Scrape Mars Weather Twitter feed to gather latest weather update
-    url = 'https://twitter.com/marswxreport?lang=en'
-    response = requests.get(url)
+    url3 = 'https://twitter.com/marswxreport?lang=en'
+    response = requests.get(url3)
     soup = bs(response.content, 'html.parser')
     results = soup.find_all('div', class_='js-tweet-text-container')
+    mars_weather = ''
     for result in results:
         if 'InSight sol' in result.text:
-        mars_weather = result.text
+            mars_weather = result.text
         break
-    time.sleep(2)
-
-    # # Scrape space-facts.com to gather table of Mars factoids
-    url = 'https://space-facts.com/mars/'
-    response = requests.get(url)
-    soup = bs(response.content, 'html.parser')
-    results = soup.find('table', id ='tablepress-p-mars-no-2')
-    tbody = results.find('tbody')
-    trow = tbody.find_all('tr')
-    table_list = []
-    for item in trow:
-        column1 = item.find('td', class_='column-1').text
-        column2 = item.find('td', class_='column-2').text
-        table_list.append({'Measurement':column1, 'Data': column2}) 
-    table_df = pd.DataFrame(table_list)
-    table_df = table_df[['Measurement','Data']]
-    table_df = table_df.set_index('Measurement')
-    table_df.to_html('facts_table.html')
     time.sleep(2)
     
     # # Scrape USGS website to get images of Mars' four hemispheres
-    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
+    url5 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url5)
     button = browser.find_by_text('Cerberus Hemisphere Enhanced')
     button.click()
     cerberus_image = browser.find_by_css('.wide-image')['src']
     cerberus_title = browser.find_by_css('.title').text
     time.sleep(2)
     
-    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
+    browser.visit(url5)
     button = browser.find_by_text('Syrtis Major Hemisphere Enhanced')
     button.click()
     syrtis_major_image = browser.find_by_css('.wide-image')['src']
     syrtis_major_title = browser.find_by_css('.title').text
-    time.sleep(1)
+    time.sleep(2)
 
-    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
+    browser.visit(url5)
     button = browser.find_by_text('Schiaparelli Hemisphere Enhanced')
     button.click()
     schiaparelli_image = browser.find_by_css('.wide-image')['src']
     schiaparelli_title = browser.find_by_css('.title').text
+    time.sleep(2)
 
-    executable_path = {'executable_path':'c:/users/ktauf/desktop/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
+    browser.visit(url5)
     button = browser.find_by_text('Valles Marineris Hemisphere Enhanced')
     button.click()
     valles_marineris_image = browser.find_by_css('.wide-image')['src']
     valles_marineris_title = browser.find_by_css('.title').text
-    
-    # # Create dataframe of Mars hemispheres images
-    hemisphere_image_urls = [
-        {"title": valles_marineris_title, "img_url": valles_marineris_image},
-        {"title": cerberus_title, "img_url": cerberus_image},
-        {"title": schiaparelli_title, "img_url": schiaparelli_image},
-        {"title": syrtis_major_title, "img_url": syrtis_major_image}]
-        hemisphere_images = pd.DataFrame(hemisphere_image_urls)
-    
-    mars_data = [
-        {'news_title' : news_title}, 
-        {'news_p': news_p}, 
-        {'featured_image_url': featured_image_url}, 
-        {'mars_weather' : mars_weather}
-        ]
-    
-    return hemisphere_image_urls, mars_data
 
+    # # Create dataframe of Mars information
+    mars_data = {
+        'news_title' : news_title, 
+        'news_subtitle': news_p, 
+        'featured_image_url': featured_image_url, 
+        'mars_weather' : mars_weather,
+        "valles_marineris_title": valles_marineris_title, 
+        "valles_marineris_img_url": valles_marineris_image,
+        "cerberus_title": cerberus_title, 
+        "cerberus_img_url": cerberus_image,
+        "schiaparelli_title": schiaparelli_title, 
+        "schiaparelli_img_url": schiaparelli_image,
+        "syrtis_major_title": syrtis_major_title, 
+        "syrtis_major_img_url": syrtis_major_image}
+    
+    return mars_data
 
-scrape_mars()
+init_browser()
+scrape()
